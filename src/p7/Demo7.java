e02e3a7a090c9f49656851c4e7ef952a91638845
emailman <emailman@dtcc.edu>
2016-03-22 13:40:06 -0400

    Added tab pane demo

--- src/p7/Demo7.java
+++ src/p7/Demo7.java
@@ -6,10 +6,9 @@ package p7;
 
 import javafx.application.Application;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
